964873599e283ce78257fd8aebcd7c6bb56e7b56
Kunal Prashant <kunalprashant77@gmail.com>
2023-07-02 17:06:20 +0530

    database connected

--- server.js
+++ server.js
@@ -1,11 +1,58 @@
-const express = require("express")
-const serverConfig = require("./configs/server.config");
+const express  = require('express');
+const serverConfig =require('./configs/server.config');
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
+const userModel = require('./models/user.model');
+
 
 const app = express();
 
-// PORT=3333;
 
-app.listen(serverConfig.PORT, ()=>{
-    console.log("Server started!")
+/**
+ * Logic to connect to MongoDB and create an ADMIN user
+ * Need to have the mongodb up and running in your local machine
+ */
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection ;
+
+db.on("error", ()=>{
+    console.log("Error while connecting to DB");
+});
+
+db.once("open", ()=>{
+    console.log("DB is connected");
+
+    init();
 })
 
+async function  init(){
+    
+    /**
+     * Check if the admin user is already present
+     */
+    let admin = await userModel.findOne({
+        userId : "admin"
+    })
+
+    if(admin){
+        console.log("Admin user already present");
+        return;
+    }
+
+    admin = await userModel.create( {
+        name : "Kunal",
+        userId : "admin",
+        email : "kunal11@gmail.com",
+        userType : "ADMIN",
+        password : "Welcome123"
+    });
+    console.log(admin);
+
+    
+}
+
+
+
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`server started on the port number ${serverConfig.PORT}` );
+})
